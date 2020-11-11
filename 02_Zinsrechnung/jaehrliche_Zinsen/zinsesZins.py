5a45c58ee58898a866bf7a4618988cece2e84e95
Andy <andy_lib@gmx.de>
2020-11-11 18:19:46 +0100

    continued

--- 02_Zinsrechnung/jaehrliche_Zinsen/zinsesZins.py
+++ 02_Zinsrechnung/jaehrliche_Zinsen/zinsesZins.py
@@ -3,15 +3,32 @@
 # ZINSESZINS -> nachschüssige Zinsen
 # Endkapital = Anfangskapital * Zinsfaktor( q = 1 + i ) ** n
 
-ANFANGSKAPITAL = 10000
-ENDKAPITAL = 10000
+ANFANGSKAPITAL = 800
+ENDKAPITAL = 1000
 JAHRE = 5
-ZINS = 0.04
+ZINS = 0.05
 ZINSFAKTOR = ZINS + 1
 
 # welches Endkapital?
 def zinsesZins(a, z, j):
     return a * z ** j
 
-print(f'{ANFANGSKAPITAL} Anfangskapital, {JAHRE} Jahre, {ZINS} Zins:')
+# wie hoch muss Anfangskapital sein?
+def getAnfangskapital(e, z, j):
+    return e / ( z ** j )
+
+# mit welchem Zins?
+def getZinssatz(e, a, j):
+    return ( e / a ) ** (1/j) - 1
+
+# für wie viele Jahre?
+# ln wird gebraucht .. lol
+
+print(f'{ANFANGSKAPITAL} Anfangskapital, {ZINS} Zins, {JAHRE} Jahre:')
 print( zinsesZins(ANFANGSKAPITAL, ZINSFAKTOR, JAHRE) )
+
+print(f'{ENDKAPITAL} Endkapital, {ZINS} Zins, {JAHRE} Jahre:')
+print( getAnfangskapital(ANFANGSKAPITAL, ZINSFAKTOR, JAHRE) )
+
+print(f'{ENDKAPITAL} Endkapital, {ANFANGSKAPITAL} Anfangskapital, {JAHRE} Jahre:')
+print( getZinssatz(ENDKAPITAL, ANFANGSKAPITAL, JAHRE) )
