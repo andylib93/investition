5d06a6152be0903a78d04afc136b2bd6b534aaa2
Andy <andy_lib@gmx.de>
2020-11-20 12:54:36 +0100

    vor- und nachschüssig

--- sparkassenFormel.py
+++ sparkassenFormel.py
@@ -5,8 +5,16 @@ ZINS = 0.08
 RATE = 6000
 JAHRE = 35
 
-def sparkassenFormel(r, z, j):
+# nachschüssig
+def sparkassenFormelN(r, z, j):
     return r * ( ((z + 1)**j - 1) / z )
 
-print(f'Rate: {RATE}, Zins: {ZINS}, Jahre: {JAHRE}:')
-print(sparkassenFormel(RATE, ZINS, JAHRE))
+# vorschüssig
+def sparkassenFormelV(r, z, j):
+    return r * (z + 1) * ( ((z + 1)**j - 1) / z )
+
+print(f'Rate: {RATE}, Zins: {ZINS}, Jahre: {JAHRE}, nachschüssig:')
+print(sparkassenFormelN(RATE, ZINS, JAHRE))
+
+print(f'Rate: {RATE}, Zins: {ZINS}, Jahre: {JAHRE}, vorschüssig:')
+print(sparkassenFormelV(RATE, ZINS, JAHRE))
